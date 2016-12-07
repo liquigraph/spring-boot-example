85203dfa51199257f0ca634c349e6d87ab3459b1
Michael Vitz <michael.vitz@innoq.com>
2016-12-07 22:16:10 +0100

    Use spring-boot-starter-liquigraph

--- src/main/java/org/liquigraph/examples/spring/MigrationConfiguration.java
+++ src/main/java/org/liquigraph/examples/spring/MigrationConfiguration.java
@@ -2,9 +2,7 @@ package org.liquigraph.examples.spring;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.liquigraph.core.api.Liquigraph;
-import org.liquigraph.core.configuration.ConfigurationBuilder;
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,28 +12,8 @@ import javax.sql.DataSource;
 class MigrationConfiguration {
 
     @Bean
-    public MethodInvokingFactoryBean liquigraph(org.liquigraph.core.configuration.Configuration liquigraphConfig) {
-        MethodInvokingFactoryBean method = new MethodInvokingFactoryBean();
-        method.setTargetObject(new Liquigraph());
-        method.setTargetMethod("runMigrations");
-        method.setArguments(new Object[] {liquigraphConfig});
-        return method;
-    }
-
-    @Bean
-    public org.liquigraph.core.configuration.Configuration configuration(DataSource dataSource) {
-        return new ConfigurationBuilder()
-                .withDataSource(dataSource)
-                .withMasterChangelogLocation("changelog.xml")
-                .withRunMode()
-                .build();
-    }
-
-    @Bean
     public DataSource dataSource() {
         HikariConfig configuration = new HikariConfig("/datasource.properties");
         return new HikariDataSource(configuration);
     }
-
-
 }
